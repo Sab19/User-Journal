b616bc17b5a77627d78684d4a7219df719448c91
Sabiya <sabiyapathan.sp@gmail.com>
2024-08-25 02:51:00 +0530

    connected project to mongDB atlas(AWS-project instance)

--- src/main/java/net/engineeringDigest/journalApp/JournalApplication.java
+++ src/main/java/net/engineeringDigest/journalApp/JournalApplication.java
@@ -2,12 +2,23 @@ package net.engineeringDigest.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class JournalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JournalApplication.class, args);
+	}
+	
+	@Bean
+	public PlatformTransactionManager mongoConnection(MongoDatabaseFactory dbFactory) {
+		return new MongoTransactionManager(dbFactory);
 	}
 
 }
--- src/main/java/net/engineeringDigest/journalApp/service/JournalEntryService.java
+++ src/main/java/net/engineeringDigest/journalApp/service/JournalEntryService.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import net.engineeringDigest.journalApp.entity.JournalEntry;
 import net.engineeringDigest.journalApp.entity.User;
@@ -21,6 +22,7 @@ public class JournalEntryService{
 	@Autowired
 	private UserService userService;
 	
+	@Transactional
 	public void saveEntry(JournalEntry journalEntry, String username) {
 		User user = userService.findByUsername(username);
 		journalEntry.setDate(LocalDateTime.now());
