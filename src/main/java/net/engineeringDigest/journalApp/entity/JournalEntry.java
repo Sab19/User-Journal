fa70261d13c8e30da55aeaa57948a22dd9d4c785
unknown <sabiyapathan.sp@gmail.com>
2024-09-02 21:15:16 +0530

    removed getter setters as we have lombok

--- src/main/java/net/engineeringDigest/journalApp/entity/JournalEntry.java
+++ src/main/java/net/engineeringDigest/journalApp/entity/JournalEntry.java
@@ -22,37 +22,4 @@ public class JournalEntry {
 	private String content;
 	
 	private LocalDateTime date;
-
-	public ObjectId getId() {
-		return id;
-	}
-
-	public void setId(ObjectId id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public LocalDateTime getDate() {
-		return date;
-	}
-
-	public void setDate(LocalDateTime date) {
-		this.date = date;
-	}
-	
 }
--- src/main/java/net/engineeringDigest/journalApp/entity/User.java
+++ src/main/java/net/engineeringDigest/journalApp/entity/User.java
@@ -32,44 +32,4 @@ public class User {
 	private List<JournalEntry> journalEntries = new ArrayList<>();
 
 	private List<String> roles;
-	
-	public ObjectId getId() {
-		return id;
-	}
-
-	public void setId(ObjectId id) {
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public List<JournalEntry> getJournalEntries() {
-		return journalEntries;
-	}
-
-	public void setJournalEntries(List<JournalEntry> journalEntries) {
-		this.journalEntries = journalEntries;
-	}
-
-	public List<String> getRoles() {
-		return roles;
-	}
-
-	public void setRoles(List<String> roles) {
-		this.roles = roles;
-	}		
 }
